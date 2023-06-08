fbb816c30bd8025f5bc686fb94e7079dba92b1fb
KhaledEllithy <khaledellessy310@gmail.com>
2023-06-09 00:38:36 +0300

    Adding Display Category Query && Table Html For Dispaly Category

---
+++ Models/categories.php
@@ -0,0 +1,50 @@
+<?php
+
+$table = 'categories';
+
+//ADD CATEGORY 
+function AddCategory($data)
+{
+    global $db;
+    global $table;
+
+    try {
+        $query = "INSERT INTO `cafe_project`. $table (`id`, `name`) VALUES (:cateId,:cateName)";
+        // var_dump($query);
+
+        ### prepare query
+        $stmt = $db->prepare($query);
+        var_dump($stmt);
+
+        $stmt->bindParam(":cateId", $data['id']);
+        $stmt->bindParam(":cateName", $data['name']);
+
+        # true --> query executed successfully
+        return $stmt->execute();
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
+}
+
+// ----------------------------------------------------------------
+
+//DISPLAY CATEGORY 
+
+function DisplayCategory()
+{
+    global $db;
+    global $table;
+
+    try {
+        $query = "SELECT * FROM `cafe_project`. $table";
+        // var_dump($query);
+
+        ### prepare query
+        $stmt = $db->prepare($query);
+        $stmt->execute();
+        $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $row;
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
+}
--- Views/categories.php
+++ Views/categories.php
@@ -1,7 +1,7 @@
 <?php
 include "../layout/head.php";
 // include "../Controllers/User/UserController.php";
-include "../Models/products/categories.php";
+include "../Models/categories.php";
 include "../connection_credits.php";
 include "../connection.php";
 // include  "../validation.php";
@@ -9,7 +9,7 @@ include "../connection.php";
 
 if (isset($_POST) && !empty($_POST)) {
     AddCategory($_POST);
-    var_dump($_POST);
+    // var_dump($_POST);
 }
 
 ?>
@@ -26,6 +26,36 @@ if (isset($_POST) && !empty($_POST)) {
 
     </form>
     <br>
+    <table class="table table-striped table-dark border-light  text-center table-bordered">
+        <thead>
+            <tr>
+                <th>id</th>
+                <th>name</th>
+                <th>Show</th>
+                <th>Edit</th>
+                <th>Delete</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            $rows = DisplayCategory();
+            // var_dump($rows);
+            foreach ($rows as $row) {
+            ?>
+                <tr>
+                    <?php foreach ($row as $field) {
+                        // var_dump($field);
+                    ?>
+                        <td><?= $field ?></td>
+                    <?php     }   ?>
+                    <td><a href="" class="btn btn-primary">Show</a></td>
+                    <td><a href="" class="btn btn-warning">Edit</a></td>
+                    <td><a href="" class="btn btn-danger">Delete</a></td>
+                </tr>
+            <?php     }   ?>
+        </tbody>
+    </table>
+
 </div>
 
 
