1f6cc620cd454f5503d29d9cdf4b1c0f15d461af
Sallyzahran <sallyzahran.sz@gmail.com>
2023-06-09 18:52:34 +0300

    modify orderController

--- Controllers/order_controller.php
+++ Controllers/order_controller.php
@@ -7,12 +7,21 @@ include '../Models/order_model.php';
 
 
 
+
+
 function all(){
 
   $orders = getAllOrders();
 
 
+  foreach ($orders as &$order) {
+    $orderProducts = getOrderProducts($order['id']);
+    $order['products'] = $orderProducts;
+  }
+
+  return $orders;
 }
+
 
 
 
