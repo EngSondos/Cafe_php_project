57d2419bc18cbe028574fa287d6c1be7365f3ba7
KhaledEllithy <khaledellessy310@gmail.com>
2023-06-18 05:35:05 +0300

    Adding Style For Display Product Admin

--- Views/products/DisplayProductsAdmin.php
+++ Views/products/DisplayProductsAdmin.php
@@ -26,64 +26,66 @@ if (isset($_GET['delete_id']) && !empty($_GET['delete_id'])) {
 <!-- <div class="header_menu">
 
 </div> -->
+<main style="width:80%;margin-left:auto">
+    <div class="container_admin_product text-center mx-auto ">
+        <div class="container p-50">
+            <div class="title_container">
+                <h1 class="text-primary mx-auto text-center my-4 title_product"> our Products </h1>
+                <a class="my-3 btn_product" href="Add Products.php">Add Product</a>
+            </div>
+            .
+            <table class="table table-striped table-hover table-Secondary border-dark  text-center table-bordered align-vertical">
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>name</th>
+                        <th>price</th>
+                        <th>Category Name</th>
+                        <th>quantity</th>
+                        <th>Status</th>
+                        <th>image</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    // $Products = DisplayAllProductsQuery();
+                    $Products = Display_All_Products_Query_With_Pagination();
+                    foreach ($Products as $row) {
+                    ?>
+                        <tr>
 
-<div class="container p-50">
-    <div class="title_container">
-        <h1 class="text-primary mx-auto text-center my-4 title_product"><i class="fa-solid fa-mug-hot"></i> our Products <i class="fa-solid fa-mug-hot"></i></h1>
-    </div>
-    <a class="btn btn-primary my-3 btn_product" href="Add Products.php">Add Product</a>
+                            <td><?= $row['id'] ?></td>
+                            <td><?= $row['name'] ?></td>
+                            <td><?= $row['price'] ?> EGP</td>
+                            <td><?= SelectCategoryByIdQuery($row['category_id'])   ?></td>
+                            <td><?= $row['quantity'] ?></td>
+                            <td> <?php
+                                    if ($row['quantity'] <= 0) {
+                                        echo  '<p class="bold" style="color:red;font-weight:700" >UnAvailable </p> ';
+                                    } else {
+                                        echo '<p class="bold" style="color:green;font-weight:700" >Available </p> ';
+                                    }
 
-    <table class="table table-striped table-hover table-Secondary border-dark  text-center table-bordered align-vertical">
-        <thead>
-            <tr>
-                <th>id</th>
-                <th>name</th>
-                <th>price</th>
-                <th>Category Name</th>
-                <th>quantity</th>
-                <th>Status</th>
-                <th>image</th>
-                <th>Edit</th>
-                <th>Delete</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            // $Products = DisplayAllProductsQuery();
-            $Products = Display_All_Products_Query_With_Pagination();
-            foreach ($Products as $row) {
+                                    ?> </td>
+                            <td> <img height="70" width="70" src="../../<?= $row['image'] ?>" alt=""> </td>
+
+                            <td><a href="UpdateProducts.php?product_id=<?= $row['id'] ?>" class="btn_edit"><i class="fa-solid fa-pen-to-square"></i></a></td>
+                            <td><a href="?delete_id=<?= $row['id'] ?>" class="btn_delete" onclick="return confirm('Are you sure you want to delete this category?')"><i class="fa-solid fa-circle-xmark"></i></a></td>
+                        <?php     }   ?>
+                        </tr>
+                </tbody>
+            </table>
+            <?php list(
+                $currentPage,
+                $total_pages,
+            ) = pagination();
+            printPages($total_pages, $currentPage, null);
+
             ?>
-                <tr>
-
-                    <td><?= $row['id'] ?></td>
-                    <td><?= $row['name'] ?></td>
-                    <td><?= $row['price'] ?> EGP</td>
-                    <td><?= SelectCategoryByIdQuery($row['category_id'])   ?></td>
-                    <td><?= $row['quantity'] ?></td>
-                    <td> <?php
-                            if ($row['quantity'] <= 0) {
-                                echo  '<p class="bold" style="color:red;font-weight:700" >UnAvailable </p> ';
-                            } else {
-                                echo '<p class="bold" style="color:green;font-weight:700" >Available </p> ';
-                            }
-
-                            ?> </td>
-                    <td> <img height="70" width="70" src="../../<?= $row['image'] ?>" alt=""> </td>
-
-                    <td><a href="UpdateProducts.php?product_id=<?= $row['id'] ?>" class="btn_edit"><i class="fa-solid fa-pen-to-square"></i></a></td>
-                    <td><a href="?delete_id=<?= $row['id'] ?>" class="btn_delete" onclick="return confirm('Are you sure you want to delete this category?')"><i class="fa-solid fa-circle-xmark"></i></a></td>
-                <?php     }   ?>
-                </tr>
-        </tbody>
-    </table>
-    <?php list(
-        $currentPage,
-        $total_pages,
-    ) = pagination();
-    printPages($total_pages, $currentPage, null);
-
-    ?>
-</div>
+        </div>
+    </div>
 </main>
 <!-- Optional: Place to the bottom of scripts -->
 </body>
